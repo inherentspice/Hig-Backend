83f672bcbaf633144234ec3cd3e95cb2c30b6f0b
Stephen Smith <stephenlehanesmith@gmail.com>
2022-10-22 11:22:37 +0800

    removed hard-coded past entries

--- index.js
+++ index.js
@@ -8,25 +8,6 @@ app.use(cors())
 app.use(express.json())
 app.use(express.static('build'))
 
-
-let beers = [
-  {
-    id: 1,
-    content: {nah: 17, alright: 1, yeah: 28},
-    date: "2022-10-20",
-  },
-  {
-    id: 2,
-    content: {nah: 27, alright: 10, yeah: 2},
-    date: "2022-10-19",
-  },
-  {
-    id: 3,
-    content: {nah: 0, alright: 2, yeah: 0},
-    date: "2022-10-18",
-  }
-]
-
 app.get("/api/beers", (request, response) => {
   Beer.find({}).then(beers => {
     response.json(beers)
